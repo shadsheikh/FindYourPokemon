4313039d7b3d6108d913e6e0d61823b572bcbf72
Shad Sheikh <66586570+shadsheikh@users.noreply.github.com>
2022-09-09 12:45:25 +0530

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -7,22 +7,41 @@ let _h1 = document.getElementById("_h1")
 let _loading = document.getElementById("loading")
 let img = `<img id="_img" src="" >`
 
-_loading.style.display="none"
-_button.addEventListener("click",async ()=>{
-    _loading.style.display="block"
-    await fetch(`https://pokeapi.co/api/v2/pokemon/${_input.value.toLowerCase()}`).then(
-        (apidata)=>{
-        return apidata.json()
-    }).then((actualdata)=>{
-        _h1.innerText=""
-        _img.src=actualdata.sprites.front_default
-        _loading.style.display="none"
-    }).catch((error)=>{
-        _h1.innerText = "Not Found"
-        _img.src=""
-        _loading.style.display="none"
-    })
+function success() {
+    if(_input.value.trim()==="") { 
+    _button.disabled = true; 
+    } else { 
+    _button.disabled = false;
+    }
+}
+
+let GetData = async () => {
+        _loading.style.display="block"
+        await fetch(`https://pokeapi.co/api/v2/pokemon/${_input.value.toLowerCase()}`).then(
+            (apidata)=>{
+            return apidata.json()
+        }).then((actualdata)=>{
+            _h1.innerText=""
+            _img.src=actualdata.sprites.front_default
+            _loading.style.display="none"
+        }).catch((error)=>{
+            _h1.innerText = "Not Found"
+            _img.src=""
+            _loading.style.display="none"
+        })
+}
+
+
+
+_input.addEventListener("keypress",async (event)=>{
+    if(event.key=="Enter" && _input.value.trim()!=""){
+        GetData()
+    }  
 })
+
+_loading.style.display="none"
+
+_button.addEventListener("click", GetData)
 
 // _button.addEventListener("click",()=>{
 //     fetch(`https://pokeapi.co/api/v2/pokemon/${_input.value}`).then(
